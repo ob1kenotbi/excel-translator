41120979aa334b2786ab2f59c987099ab437f1fc
Syazwan Suhaimi <wantabung@gmail.com>
2024-11-21 21:34:14 +0800

    read each excel in directory and translate

--- excel-translator.py
+++ excel-translator.py
@@ -1,20 +1,27 @@
+import os
 import openpyxl
 from deep_translator import GoogleTranslator
 
-workbook = openpyxl.load_workbook('1.xlsx')
-translator = GoogleTranslator(source='ja', target='en')
+#start read excel
+for filename in os.listdir():
+    if filename.endswith(".xlsx") or filename.endswith(".xls"):
+        print(filename)
+        #start translate
+        workbook = openpyxl.load_workbook(filename)
+        translator = GoogleTranslator(source="ja", target="en")
 
-for sheet in workbook.sheetnames:
-    worksheet = workbook[sheet]
-    
-    for row in worksheet.iter_rows():
-        for cell in row:
-            if isinstance(cell.value, str):
-                try:
-                    cell.value = translator.translate(cell.value)
-                except Exception as e:
-                    print(f"Error translating cell {cell.coordinate}: {e}")
+        for sheet in workbook.sheetnames:
+            worksheet = workbook[sheet]
+            
+            for row in worksheet.iter_rows():
+                for cell in row:
+                    if isinstance(cell.value, str):
+                        try:
+                            cell.value = translator.translate(cell.value)
+                        except Exception as e:
+                            print(f"Error translating cell {cell.coordinate}: {e}")
 
-workbook.save('translated_1.xlsx')
+        translated_filename = f"translated_{filename}"
+        workbook.save(translated_filename)
 
-print("Translation completed and saved to 'translated_1.xlsx'")
+        print(f"Translation completed and saved to {translated_filename}")
